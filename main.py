3b22d285e98495148bd63865ed2daa9b5cdde87f
mikel <gimikele@gmail.com>
2022-01-13 01:13:22 +0100

    Genetic Algorithm Added

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import DataStructure as dt
 import BruteForce as bt
 import timeit
+import GeneticAlgorithm as ga
 import ObjectiveResult as result
+
+
 
 struct2 = dt.Structure(3,"Proof/Proof.txt","Proof/ProofNames.txt")
 struct2.randomInitialization()
 struct2.print()
-struct2.updateX(bt.BruteForce(struct2.y,struct2.n))
-t1 = timeit.default_timer()
+#struct2.updateX(bt.BruteForce(struct2.y,struct2.n))
+#struct2.print()
+struct2.updateX(ga.geneticAlgorithm(struct2.y,struct2.n,500))
 struct2.print()
-t2 = timeit.default_timer()
-print("Denbora: ", t2-t1)
